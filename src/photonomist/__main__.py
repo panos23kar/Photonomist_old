97dddab6f1947277c84aefb19b765c0b36137193
panos23kar <panos23kar@gmail.com>
2020-09-20 13:49:47 +0200

    path_string and clean_path & unit tests

--- src/photonomist/__main__.py
+++ src/photonomist/__main__.py
@@ -4,23 +4,55 @@
 
 """
 import os
+
+def path_string(photo_path:str):
+    """Verifies that the provided path is string
+
+    :param photo_path: path to photos
+    :return: photo to paths
+    """
+    if (photo_path is None) or (type(photo_path) != str) or (len(photo_path)<3):
+        raise Exception("Your input is not valid!")
+    return photo_path
+
+def clean_path(photo_path:str)->str:
+    """Removes second quotes/apostrophes in case that a user has typed/pasted the path with quote.
+
+    :param photo_path: 'raw' path to photos
+    :return: path to photos without redundant quotes
+    """
+    if ord(photo_path[0]) == 34 or ord(photo_path[0])==39:
+        photo_path = photo_path[1:]
+    if ord(photo_path[-1]) == 34 or ord(photo_path[-1])==39:
+        photo_path = photo_path[:-1]
+    return photo_path
+
 def path_exists(photo_path:str):
     """Verifies that the provided path exists.
 
     :param photo_path: path to photos
     """
-    if (photo_path is None) or (not os.path.exists(photo_path)) or (type(photo_path)!=str):
+    if not os.path.exists(photo_path):
         raise FileNotFoundError("The provided path was not found!")
+
+def path_items(photo_path:str):
+    """Verifies that the provided path contains items (not only photos).
+
+    :param photo_path: path to photos
+    """
+    if not os.listdir(photo_path):
+        raise Exception("The provided path does not contain any files!")
     
 def main():
     """ Execute the application.
 
     It asks the user to specify the path to the photos.
-    It assesses the validity of the provided path and verifies that there is enough storage space.
+    It assesses the validυity of the provided path and verifies that there is enough storage space.
 
     """
-    photo_path = input("Enter the path to your photos: ")
+    photo_path = clean_path(path_string(input("Enter the path to your photos: ")))
     path_exists(photo_path)
+    path_items(photo_path)
 
 
 # Make the script executable.
--- test/photonomist/test__main__.py
+++ test/photonomist/test__main__.py
@@ -9,16 +9,50 @@ precedence over the version in this project directory. Use a virtualenv test
 environment or setuptools develop mode to test against the development version.
 """
 import pytest
-from photonomist.__main__ import path_exists
+from photonomist.__main__ import path_exists, path_items, clean_path, path_string
 
 @pytest.mark.parametrize("sample_path", [("blablabla"), 
-                                         (0), 
-                                         (None), 
-                                         (1)])
+                                         (r'C:\repos\photonomist\test\data\blablabla'), 
+                                         (r'C:/repos/photonomist/test\data/blablabla')])
 def test_invalid_path(sample_path):
+    """ Test for src\\photonomist\\__main__ > path_exists
+
+    Parametrized to test invalid str and paths
+    """
     with pytest.raises(FileNotFoundError, match="The provided path was not found!"):
         path_exists(sample_path)
 
+def test_path_contains_photos():
+    """ Test for src\\photonomist\\__main__ > path_items
+
+    Testing_empty_folder was created in test\\data for testing purposes.
+    """
+    #Need an empty dir for testing
+    sample_path = r'test\data\testing_empty_folder'
+    with pytest.raises(Exception, match="The provided path does not contain any files!"):
+        path_items(sample_path)
+
+def test_clean_path():
+    """Test src\\photonomist\\__main__ > clean_path
+    """
+    sample_path = r"'C:\repos\photonomist\test\data\testing_empty_folder'"
+    sample_path = clean_path(sample_path)
+    assert sample_path == r"C:\repos\photonomist\test\data\testing_empty_folder"
+
+@pytest.mark.parametrize("sample_path", [(0), 
+                                         (None), 
+                                         (1),
+                                         (1.0),
+                                         (True),
+                                         (False),
+                                         ('0')])
+def test_user_input_is_string(sample_path):
+    """Test src\\photonomist\\__main__ > path_string
+
+    Parametrized to test invalid int, floats, boolean and NoneType arguments.
+    """
+    with pytest.raises(Exception, match="Your input is not valid!"):
+        path_string(sample_path)
 
 # Make the script executable.
 if __name__ == "__main__":
