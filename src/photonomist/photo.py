4ed75c68c64c58aba1f9d10639e0f301b39dab11
panos23kar <panos23kar@gmail.com>
2020-10-03 11:15:29 +0200

    Doc and minor changes to Photo class

--- src/photonomist/photo.py
+++ src/photonomist/photo.py
@@ -6,8 +6,8 @@ import os, shutil
 
 class Photo:
     """This class is used to represent a photo.
-    A photo object holds a photo's metadata and methods to return it.
-    It can also move itself to another directory
+    A photo object holds a photo's metadata.
+    It can also move itself to another directory and return its captured date.
 
     :param photo_path: Path to photo
     :type photo_path: str
@@ -20,7 +20,7 @@ class Photo:
         self.__metadata_dict()
     
     def __str__(self)->str:
-        """Returns the string representation (name) of the photo
+        """Returns the name of the photo.
 
         :return: name of the photo
         :rtype: str
@@ -31,15 +31,17 @@ class Photo:
         """Extracts the metadata tags from a photo using `exifread <https://exif-py.readthedocs.io/en/latest/>`_ library.
         It contains duplicate values with different tags.
         """
-        self.tags = {}
+        self.__tags = {}
         try:
             photo_file = open(self.path, 'rb')
-            self.tags = exifread.process_file(photo_file, details=False)
-        except:
+            # details=False --> Faster Processing: Don’t process makernote tags, don’t extract the thumbnail image (if any).
+            self.__tags = exifread.process_file(photo_file, details=False)
+        except: #TODO Log it
             print("I didn't manage to extract photo's tags!")
 
     def __metadata_dict(self):
-        """Populates the metadata dictionary with all key tags that contain values.
+        """Populates the metadata dictionary with all key/tags (extracted using the 
+        `exifread <https://exif-py.readthedocs.io/en/latest/>`_ library) that contain values.
         It also avoids duplicate keys with different tags:
 
         | *Example:*
@@ -49,14 +51,18 @@ class Photo:
         self.__extract_exif_tags()
         self.metadata = {}
         
-        if self.tags:
-            for tag_key in self.tags.keys():
+        if self.__tags:
+            for tag_key in self.__tags.keys():
+                # Image DateTimeOriginal = 2019:12:14 15:04:33 --> tag = Image || key = DateTimeOriginal || value = 2019:12:14 15:04:33
                 key_no_tag = tag_key.split()[1]
-                if (len(str(self.tags[tag_key]))>0) and (key_no_tag not in self.metadata): #len(str(self.tags[tag_key]))>0 because ``if self.tags[tag_key]`` doesn't work
-                    self.metadata[key_no_tag] = self.tags[tag_key]
+
+                #len(str(self.__tags[tag_key]))>0 because self.__tags[tag_key] doesn't work
+                if (len(str(self.__tags[tag_key]))>0) and (key_no_tag not in self.metadata):
+                    self.metadata[key_no_tag] = self.__tags[tag_key]
 
     def get_date(self)->str:
-        """Returns the date of a photo from the metadata dictionary
+        """Returns the date of a photo from the metadata dictionary. 
+        As date considered the value of the "DateTimeOriginal" tag. 
         
         :return: date of photo
         :rtype: str
@@ -68,10 +74,10 @@ class Photo:
             return None
     
     def move_to_folder(self, new_folder_path:str):
-        """Moves the photo to a new directory.
-        If directory doesn't exist it creates one
+        """Moves the photo to another directory.
+        If directory doesn't exist it creates one.
 
-        :param new_folder_path: the path to dir where the photo will be moved to
+        :param new_folder_path: the path to directory
         :type new_folder_path: str
         """
         new_path = os.path.join(new_folder_path, self.__str__())        
@@ -80,17 +86,10 @@ class Photo:
         except:
             os.makedirs(new_folder_path)
             shutil.move(self.path, new_path)
+        finally:
             self.path = new_path
 
 
 if __name__ == "__main__":
-    #kati = Photo(r"C:\repos\photonomist\test\data\testing_folder_with_photos\bla\DSC_0262.NEF")
-    #kati.extract_exif_tags()
-    #kati.metadata_dict()
-    # for key,value in kati.metadata.items():
-    #     print(f"key--> {key} \t\t value--> {value}")
-    #print(kati)
-    #kati.move_to_folder(r"C:\repos\photonomist\test\data\testing_folder_with_photos\move_folder")
-    #photo_path = r"C:\Users\potis\Pictures\2016\2016-08-31\IMG-20160831-WA0020.jpg"
-    photo_path = r"C:\Users\potis\Pictures\Lg_G3_14-12-2016\Facebook\FB_IMG_1470137678394.jpg"
+    photo_path = Photo(r"C:\repos\photonomist\test\data\testing_folder_with_photos\bla\DSC_0262.NEF")
     my_photo = Photo(photo_path).get_date()
