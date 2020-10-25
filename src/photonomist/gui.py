10710d56a30f4e03ef0cb3b042fd772d56c065f7
panos23kar <panos23kar@gmail.com>
2020-10-25 12:09:51 +0100

    Gui class with doc

--- src/photonomist/gui.py
+++ src/photonomist/gui.py
@@ -3,6 +3,33 @@ This file hosts the graphical user interface code"""
 
 import tkinter as tk
 
-gui = tk.Tk()
-gui.title("Photonomist")
-gui.mainloop()
+class Gui:
+    """This class is used to "draw" the graphical user interface through which 
+    users interact with photonomist.
+
+    |
+    """
+    def __init__(self):
+        """Constructor method
+        |
+        """
+        self.__gui = tk.Tk()
+        self.__main_window()
+
+        self.start_gui()
+    
+    def __main_window(self):
+        """
+        Specifies the title and dimensions of main window
+        """
+        self.__gui.title("Photonomist")
+        self.__gui.geometry("500x500")
+    
+    def start_gui(self):
+        """
+        Starts the graphical user interface
+        """
+        self.__gui.mainloop()
+
+if __name__ == "__main__":
+    Gui()
