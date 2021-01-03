db48e11528bc53bb63bb22ca042a98b4d4aa7ccb
panos23kar <panos23kar@gmail.com>
2021-01-03 17:41:22 +0100

    fixed default values for name_pattern arg

--- src/photonomist/__main__.py
+++ src/photonomist/__main__.py
@@ -6,7 +6,7 @@
 """
 import os, shutil, subprocess
 import collections
-from photo import Photo
+from .photo import Photo
 
 
 def path_string(path:str)->str:
@@ -139,7 +139,7 @@ def disk_space(export_path:str, photos_total_size:int):
     else:
         raise Exception(f"You need at least {photos_total_size + 1073741824} free bytes but you only have {free} avaialable!")#TODO Log it
 
-def photo_dir_name(date:str, year:bool=False, month:bool=False, name_pattern:str="")->str:
+def photo_dir_name(date:str, year:bool=False, month:bool=False, name_pattern:str="_place_reason_people")->str:
     """Generates the name of the folder where the photos will be moved according to their dates.
     
     | **Name pattern**: *year_month_day_place_reason_people*
@@ -208,7 +208,7 @@ def write_not_transferred_photos(photo_path:str, export_path:str):
     with open(os.path.join(export_path, "not_transferred.txt"), "a") as myfile:
         myfile.write(photo_path + "\n")
 
-def transfer_photo(photo_path:str, export_path:str, year:bool=False, month:bool=False, name_pattern:str=""):
+def transfer_photo(photo_path:str, export_path:str, year:bool=False, month:bool=False, name_pattern:str="_place_reason_people"):
     """Moves a photo to a "date" folder, if a date was extracted.
 
     :param photo_path: path to photo
@@ -274,7 +274,7 @@ def export_path_validation(export_path:str, photos_path:str, photos_roots:dict):
         photos_total_size = photos_size(photos_roots)
         disk_space(export_path, photos_total_size)
 
-def tidy_photos(export_path:str, photos_roots:dict, year:bool=False, month:bool=False, name_pattern:str=""):
+def tidy_photos(export_path:str, photos_roots:dict, year:bool=False, month:bool=False, name_pattern:str="_place_reason_people"):
     """Initiates the transfer process for each photo
 
     :param export_path: path to the directory where the photo folder structure will be created
