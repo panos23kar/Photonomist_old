0258511b39bc871e4db0bf1652212288a9292465
panos23kar <panos23kar@gmail.com>
2021-01-17 15:50:01 +0100

    create __read_loading_image_bytestream in loading_window

--- src/photonomist/gui/loading_window.py
+++ src/photonomist/gui/loading_window.py
@@ -13,22 +13,41 @@ class LoadingWindow():
     def start_load_w_thread(self, func2run):
         self.__load_widnow_thread = Thread(target=func2run)
         self.__load_widnow_thread.start()
-
+        # Loading Image window while photonomist is working on user's request
         self.__check_thread()
-        #self.__loading_window.after(50, self.__check_thread)
     
     def __close_toplevel(self):
         self.__loading_window.destroy()
         self.__loading_window.update()
     
     def __check_thread(self):
-
         if not self.__load_widnow_thread.is_alive():
             self.__close_toplevel()
         else:
             self.__load_w_layout()
             self.__update_load_w = self.__draw_loading_camera().__next__
-            self.__load_w_canvas.after(100, self.__update_load_w)            
+            self.__load_w_canvas.after(100, self.__update_load_w)
+
+    def __read_loading_image_bytestream(self):
+        try:
+            with open(r'src\photonomist\gui\load_image_bytes.json') as image_bytestream:
+                self.__filename = json.load(image_bytestream)["data"]
+        except:
+            pass
+
+
+    def __load_w_layout(self):
+        # Loading window image bytestream
+
+        # Load window cconfiguration
+        self.__loading_window = tk.Toplevel(self.__gui)
+        self.__loading_window.title("I'm working on it!!")
+        ## Load window gets the 'full' focus of the app
+        self.__loading_window.grab_set()
+
+        ##Canvas for Load window (it is need for scrolling (scrollbar) functionality)
+        self.__load_w_canvas = tk.Canvas(self.__loading_window,  width=500, height=500)
+        self.__load_w_canvas.pack()      
     
     def __draw_loading_camera(self):
         image = Image.open(BytesIO(base64.b64decode(self.__filename)))
@@ -46,19 +65,3 @@ class LoadingWindow():
         
         self.__loading_window.destroy()
         self.__loading_window.update()
-    
-    def __load_w_layout(self):
-        # Loading window image bytestream
-        with open(r'src\photonomist\gui\load_image_bytes.json') as image_bytestream:
-            data = json.load(image_bytestream)
-            self.__filename = data["data"]
-
-        # Load window cconfiguration
-        self.__loading_window = tk.Toplevel(self.__gui)
-        self.__loading_window.title("I'm working on it!!")
-        ## Load window gets the 'full' focus of the app
-        self.__loading_window.grab_set()
-
-        ##Canvas for Load window (it is need for scrolling (scrollbar) functionality)
-        self.__load_w_canvas = tk.Canvas(self.__loading_window,  width=500, height=500)
-        self.__load_w_canvas.pack()
